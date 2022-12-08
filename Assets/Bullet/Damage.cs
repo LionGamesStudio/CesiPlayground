537832438a0b5c501a9670e9492b624c7037f3ad
eraflo <florian.maillard2003@gmail.com>
2022-12-08 15:05:08 +0100

    systeme de damage sur les cibles

---
+++ Assets/Bullet/Damage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+
+    public int damage = 50;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
