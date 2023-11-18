71b5345b31dd7e4552d24248a0e77281fbdf8526
fm <81640753+eraflo@users.noreply.github.com>
2023-11-18 19:00:23 +0100

    loading scene and upscaling the main scene

---
+++ Assets/Scripts/Scene/ScenesManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Scene
+{
+    
+    public class ScenesManager : MonoBehaviour
+    {
+        public static ScenesManager Instance;
+
+        // List of scene to instantiate
+        [SerializeField] private List<string> _scenes;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            foreach(string scene in _scenes)
+            {
+                StartCoroutine(InstantiateScene(scene));
+            }
+        }
+
+        public IEnumerator InstantiateScene(string scene)
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+
+            while(!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+        }
+
+
+    }
+}
